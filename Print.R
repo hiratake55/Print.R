c7c1e915559a95326e321d150cbc42283739a1d6
Takekatsu HIRAMURA <thira.plavox.info>
2010-11-21 18:53:42 +0900

    test

---
+++ Print.R
@@ -0,0 +1,13 @@
+Print <- function(...){
+ key <- as.list(substitute(list(...)))[-1L]
+ val <- list(...)
+ mapply(
+  function(k, v){
+   cat(k, "= ")
+   if(!is.matrix(v) && (is.logical(v) || is.numeric(v) || is.complex(v) || is.character(v))){ cat(v, "\n") }
+   else{ cat("\n"); print(v); cat("\n") }
+  },  
+  key, val)
+ cat("\n")
+}
+
