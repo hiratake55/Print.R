afa3b9a79ad5d952ccd06cbf7bfe257fac00a64c
hiratake55 <hiratake55@gmail.com>
2012-02-17 00:03:51 +0900

    Update Print.R

--- Print.R
+++ Print.R
@@ -1,16 +1,39 @@
-# Print.R
-#
+Print <- function(..., sep="\n", digits=NULL){
+ #
+ # Print (2010.08.09, 2011.10.28,29,30)
+ #
+ # (C) Takekatsu Hiramura and Shin-ichi Mayekawa
+ #
+ # Usage
+ #
+ #     Print( var1, var2, ...., varN, sep="character", digits=number )
+ #      where var.. can be of any type.
+ #
+ #   When var is either matrix or list, sep="\n" will be used.
+ #
+ #   Note that  format( var, digits=NULL ) is equal to format( var )
+ #
 
-Print <- function(...){
- key <- as.list(substitute(list(...)))[-1L]
- val <- list(...)
- mapply(
+  key <- as.list(substitute(list(...)))[-1L]
+  val <- list(...)
+
+  mapply(
+
   function(k, v){
-   cat(k, "= ")
-   if(!is.matrix(v) && (is.logical(v) || is.numeric(v) || is.complex(v) || is.character(v))){ cat(v, "\n") }
-   else{ cat("\n"); print(v); cat("\n") }
-  },  
-  key, val)
- cat("\n")
-}
 
+   cat(deparse(k), "= ")
+   if(!is.matrix(v) && (is.logical(v) || is.numeric(v) || is.complex(v)
+                                      || is.character(v))){
+    # single numeric, complex, logical, character or vector
+    cat(format(v,digits=digits),sep)
+   } else{
+    # matrix and list
+    cat("\n"); print(v, digits=digits); cat("\n")
+   }
+  },
+
+  key, val) # end of mapply
+
+  cat("\n")
+
+} # end of Print
